a0a7b61e2f377fd37a41b8f6bfdcf0c5ed22deb2
theayusharma <protocolayush@gmail.com>
2025-08-15 00:35:59 +0530

    polyglot

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -50,7 +50,6 @@ return {
 },
 
   
-
 {
   "neovim/nvim-lspconfig",
   dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
@@ -221,5 +220,29 @@ end,
         }
       })
     end
-  }
+  },
+{
+	  'sheerun/vim-polyglot',
+	  config = function()
+		vim.g.polyglot_disabled = {}  
+	  end
+	},
+	{
+		"sbdchd/neoformat", 
+		config = function()
+		  vim.cmd([[
+			augroup fmt
+			  autocmd!
+			  autocmd BufWritePre * undojoin | Neoformat
+			augroup END
+		  ]])
+
+		  vim.g.neoformat_enabled_python = {'black'}
+		  vim.g.neoformat_enabled_javascript = {'prettier'}
+		  vim.g.neoformat_enabled_lua = {'stylua'}
+
+		  vim.g.neoformat_verbose = 1
+		  vim.g.neoformat_run_all_formatters = 1
+		end,
+	  },
 }
