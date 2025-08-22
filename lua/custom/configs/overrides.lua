83d4ccc141d2c427d4cc90b38ae2d3361f56c191
theayusharma <protocolayush@gmail.com>
2025-08-23 04:09:40 +0530

    formatter

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -5,4 +5,13 @@ M.mason = {
 	ensure_installed = { "tree-sitter-cli" }
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatOnSave", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup,
+  pattern = "*",
+  callback = function(args)
+    vim.lsp.buf.format({ async = true, bufnr = args.buf })
+  end,
+})
+
 return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,91 +1,94 @@
 local overrides = require("custom.configs.overrides")
 
 return {
-{
- "echasnovski/mini.nvim", 
- version = false,
- config = function()
-   require('mini.pairs').setup()
- end
-},
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+      require('mini.pairs').setup()
+    end
+  },
 
-{
- "mattn/emmet-vim",
- ft = { "html", "css", "javascript", "typescript", "jsx", "tsx" },
-},
-{
-		"windwp/nvim-autopairs",
-		-- event = "InsertEnter",
-		-- opts = function() return require("config.nvim-autopairs") end,
-		config = function(_, opts)
-			require("nvim-autopairs").setup(opts)
-			local cmp_status_ok, cmp = pcall(require, "cmp")
-			local cmp_autopairs_status_ok, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
-			if cmp_status_ok and cmp_autopairs_status_ok then
-				cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
-			end
-		end,
-	},
-{
-  "windwp/nvim-ts-autotag",
-  dependencies = "nvim-treesitter/nvim-treesitter",
-  ft = { "html", "javascript", "typescript", "jsx", "tsx", "vue", "svelte" },
-  config = function()
-    require('nvim-ts-autotag').setup({
-      opts = {
-        enable_close = true,
-        enable_rename = true,
-        enable_close_on_slash = false
-      },
-      per_filetype = {
-        ["html"] = {
-          enable_close = true
+  {
+    "mattn/emmet-vim",
+    ft = { "html", "css", "javascript", "typescript", "jsx", "tsx" },
+  },
+  {
+    "windwp/nvim-autopairs",
+    -- event = "InsertEnter",
+    -- opts = function() return require("config.nvim-autopairs") end,
+    config = function(_, opts)
+      require("nvim-autopairs").setup(opts)
+      local cmp_status_ok, cmp = pcall(require, "cmp")
+      local cmp_autopairs_status_ok, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+      if cmp_status_ok and cmp_autopairs_status_ok then
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
+      end
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    ft = { "html", "javascript", "typescript", "jsx", "tsx", "vue", "svelte" },
+    config = function()
+      require('nvim-ts-autotag').setup({
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false
+        },
+        per_filetype = {
+          ["html"] = {
+            enable_close = true
+          }
+        }
+      })
+    end,
+  },
+
+  {
+    "s1n7ax/nvim-window-picker",
+    version = "v1.*",
+    config = function()
+      require("window-picker").setup({
+        -- Your window-picker configuration (optional)
+        hint = "floating-big-letter",
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = true,
+          bo = {
+            filetype = { "neo-tree", "neo-tree-popup", "notify" },
+            buftype = { "terminal", "quickfix" },
+          },
+        },
+      })
+    end,
+  },
+  --goated typescript thing, never made me sad
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+    config = function()
+      require("typescript-tools").setup {
+        on_attach = function(client, bufnr)
+          print("TypeScript Tools attached to buffer " .. bufnr)
+        end,
+        settings = {
+          formatter = "prettier",
+          separate_diagnostic_server = true,
+          publish_diagnostic_on = "insert_leave",
+          -- tsserver_path = ".npm/install/global/node_modules/typescript/bin/tsserver",
+
+          tsserver_path = vim.fn.expand("$HOME") .. "/.bun/install/global/node_modules/typescript/bin/tsserver",
         }
       }
-    })
-  end,
-},
-
-{
-"s1n7ax/nvim-window-picker",
-version = "v1.*",
-config = function()
-  require("window-picker").setup({
-    -- Your window-picker configuration (optional)
-    hint = "floating-big-letter",
-    filter_rules = {
-      include_current_win = false,
-      autoselect_one = true,
-      bo = {
-        filetype = { "neo-tree", "neo-tree-popup", "notify" },
-        buftype = { "terminal", "quickfix" },
-      },
-    },
-  })
-end,
-},
-  --goated typescript thing, never made me sad
-{
-  "pmizio/typescript-tools.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "neovim/nvim-lspconfig",
+    end,
   },
-  ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-  config = function()
-    require("typescript-tools").setup {
-      on_attach = function(client, bufnr)
-        -- Optional: Add keymaps or other setup here
-        print("TypeScript Tools attached to buffer " .. bufnr)
-      end,
-      settings = {
-        separate_diagnostic_server = true,
-        publish_diagnostic_on = "insert_leave",
-      }
-    }
-  end,
-},
-{
+  {
     "catgoose/nvim-colorizer.lua",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
@@ -128,103 +131,103 @@ end,
       })
     end,
   },
-	-- {
-	-- 	"sbdchd/neoformat", 
-	-- 	config = function()
-	-- 	  vim.cmd([[
-	-- 		augroup fmt
-	-- 		  autocmd!
-	-- 		  autocmd BufWritePre * undojoin | Neoformat
-	-- 		augroup END
-	-- 	  ]])
-	--
-	-- 	  vim.g.neoformat_enabled_python = {'black'}
-	-- 	  vim.g.neoformat_enabled_javascript = {'prettier'}
-	-- 	  vim.g.neoformat_enabled_lua = {'stylua'}
-	--
-	-- 	  vim.g.neoformat_verbose = 1
-	-- 	  vim.g.neoformat_run_all_formatters = 1
-	-- 	end,
-	--   },
--- {
---   'smoka7/multicursors.nvim',
---   event = 'VeryLazy',
---   dependencies = {
---     'nvim-treesitter/nvim-treesitter',
---     'nvim-lua/plenary.nvim',
---     'nvimtools/hydra.nvim',
---   },
---   opts = {
---     -- Enable hints (this is the key setting!)
---     generate_hints = {
---       normal = true,
---       insert = true, 
---       extend = true,
---       config = {
---         column_count = 1,           -- Single column for cleaner look
---         max_hint_length = 30,       -- Longer descriptions
---       }
---     },
---     -- Customize hint window appearance
---     hint_config = {
---       float_opts = {
---         border = 'rounded',         -- Nice border
---       },
---       position = 'bottom-right',    -- Position like which-key
---     },
---     -- Keep default keymaps simple
---     normal_keys = {},  -- Use defaults
---     insert_keys = {},  -- Use defaults  
---     extend_keys = {},  -- Use defaults
---   },
---   cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor', 'MCword' },
---   keys = {
---     -- Simple VS Code-like keybindings
---     {
---       '<C-n>',
---       '<cmd>MCstart<cr>',
---       mode = 'n',
---       desc = 'Add cursor on word',
---     },
---     {
---       '<C-n>',
---       '<cmd>MCvisual<cr>',
---       mode = 'v', 
---       desc = 'Add cursor on selection',
---     },
---     {
---       '<leader>ma',
---       '<cmd>MCpattern<cr>',
---       mode = 'n',
---       desc = 'Add cursors by pattern',
---     },
---     {
---       '<leader>mc',
---       '<cmd>MCclear<cr>',
---       mode = 'n',
---       desc = 'Clear all cursors',
---     },
---   },
--- },
+  {
+    "sbdchd/neoformat",
+    config = function()
+      vim.cmd([[
+			augroup fmt
+			  autocmd!
+			  autocmd BufWritePre * undojoin | Neoformat
+			augroup END
+		  ]])
+
+      vim.g.neoformat_enabled_python = { 'black' }
+      vim.g.neoformat_enabled_javascript = { 'prettier' }
+      vim.g.neoformat_enabled_lua = { 'stylua' }
+
+      vim.g.neoformat_verbose = 1
+      vim.g.neoformat_run_all_formatters = 1
+    end,
+  },
+  -- {
+  --   'smoka7/multicursors.nvim',
+  --   event = 'VeryLazy',
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'nvim-lua/plenary.nvim',
+  --     'nvimtools/hydra.nvim',
+  --   },
+  --   opts = {
+  --     -- Enable hints (this is the key setting!)
+  --     generate_hints = {
+  --       normal = true,
+  --       insert = true,
+  --       extend = true,
+  --       config = {
+  --         column_count = 1,           -- Single column for cleaner look
+  --         max_hint_length = 30,       -- Longer descriptions
+  --       }
+  --     },
+  --     -- Customize hint window appearance
+  --     hint_config = {
+  --       float_opts = {
+  --         border = 'rounded',         -- Nice border
+  --       },
+  --       position = 'bottom-right',    -- Position like which-key
+  --     },
+  --     -- Keep default keymaps simple
+  --     normal_keys = {},  -- Use defaults
+  --     insert_keys = {},  -- Use defaults
+  --     extend_keys = {},  -- Use defaults
+  --   },
+  --   cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor', 'MCword' },
+  --   keys = {
+  --     -- Simple VS Code-like keybindings
+  --     {
+  --       '<C-n>',
+  --       '<cmd>MCstart<cr>',
+  --       mode = 'n',
+  --       desc = 'Add cursor on word',
+  --     },
+  --     {
+  --       '<C-n>',
+  --       '<cmd>MCvisual<cr>',
+  --       mode = 'v',
+  --       desc = 'Add cursor on selection',
+  --     },
+  --     {
+  --       '<leader>ma',
+  --       '<cmd>MCpattern<cr>',
+  --       mode = 'n',
+  --       desc = 'Add cursors by pattern',
+  --     },
+  --     {
+  --       '<leader>mc',
+  --       '<cmd>MCclear<cr>',
+  --       mode = 'n',
+  --       desc = 'Clear all cursors',
+  --     },
+  --   },
+  -- },
 
   {
     "jake-stewart/multicursor.nvim",
     branch = "1.0",
     keys = {
-      { "<C-n>", mode = { "n", "x" } },
-      { "<S-C-n>", mode = { "n", "x" } },
-      { "<leader>s", mode = { "n", "x" } },
-      { "<leader>S", mode = { "n", "x" } },
-      { "<Up>", mode = { "n", "x" } },
-      { "<Down>", mode = { "n", "x" } },
-      { "<leader><Up>", mode = { "n", "x" } },
-      { "<leader><Down>", mode = { "n", "x" } },
-      { "<C-q>", mode = { "n", "x" } },
-      { "<Left>", mode = { "n", "x" } },
-      { "<Right>", mode = { "n", "x" } },
-      { "<leader>x", mode = { "n", "x" } },
-      { "<C-LeftMouse>", mode = { "n" } },
-      { "<C-LeftDrag>", mode = { "n" } },
+      { "<C-n>",           mode = { "n", "x" } },
+      { "<S-C-n>",         mode = { "n", "x" } },
+      { "<leader>s",       mode = { "n", "x" } },
+      { "<leader>S",       mode = { "n", "x" } },
+      { "<Up>",            mode = { "n", "x" } },
+      { "<Down>",          mode = { "n", "x" } },
+      { "<leader><Up>",    mode = { "n", "x" } },
+      { "<leader><Down>",  mode = { "n", "x" } },
+      { "<C-q>",           mode = { "n", "x" } },
+      { "<Left>",          mode = { "n", "x" } },
+      { "<Right>",         mode = { "n", "x" } },
+      { "<leader>x",       mode = { "n", "x" } },
+      { "<C-LeftMouse>",   mode = { "n" } },
+      { "<C-LeftDrag>",    mode = { "n" } },
       { "<C-LeftRelease>", mode = { "n" } },
     },
     config = function()
