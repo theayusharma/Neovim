78d8b258bba1550c3bc288ccf1fb445019ca8dd4
theayusharma <protocolayush@gmail.com>
2025-09-23 04:53:19 +0530

    not working with fzf

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,5 +1,23 @@
+-- vim.keymap.set("n", "<A-Tab>", function()
+--   require("telescope.builtin").buffers({
+--     sort_mru = true,
+--     ignore_current_buffer = true,
+--     previewer = false,
+--     layout_strategy = "vertical",
+--     layout_config = {
+--       height = 0.4,
+--       width = 0.5,
+--     },
+--     attach_mappings = function(prompt_bufnr, map)
+--       map("i", "<A-Tab>", "move_selection_next")
+--       map("n", "<A-Tab>", "move_selection_next")
+--       return true
+--     end,
+--   })
+-- end, { desc = "Switch buffers" })
+
 vim.keymap.set("n", "<A-Tab>", function()
-  require("telescope.builtin").buffers({
+  require("fzf-lua").buffers({
     sort_mru = true,
     ignore_current_buffer = true,
     previewer = false,
@@ -15,4 +33,5 @@ vim.keymap.set("n", "<A-Tab>", function()
     end,
   })
 end, { desc = "Switch buffers" })
+
 vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
