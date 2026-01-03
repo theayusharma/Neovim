c6f7a6e78aa333b5e5e1ee40dd3bfeea5e2421d1
theayusharma <protocolayush@gmail.com>
2026-01-04 02:53:49 +0530

    testing bun for tsserver, didn't works

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -6,6 +6,28 @@ return {
   },
   ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
   config = function()
-    require("typescript-tools").setup({})
+    require("typescript-tools").setup({
+      on_attach = function(client)
+        --conform x biome for formatting not this
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+      settings = {
+        separate_diagnostic_server = true,
+        publish_diagnostic_on = "insert_leave",
+
+        --old stuff
+        -- tsserver_path = ".npm/install/global/node_modules/typescript/bin/tsserver",
+        -- tsserver_path = vim.fn.expand("$HOME") .. "/.bun/install/global/node_modules/typescript/bin/tsserver",
+        -- tsserver_path = "bunx --bun typescript-language-server --stdio",
+        -- tsserver_path = vim.fn.expand("$HOME") .. "/.bun/install/global/node_modules/typescript/lib/tsserver",
+
+        -- node tsserver takes 109Mb vs bun  shebang takes 89Mb, no performance gains i guesss but yay bun
+        -- bun shebang override tsserver
+        -- --this doesn't work either, can't fundamentally remove node from tsserver
+        tsserver_path = "bun --bun $HOME/.bun/bin/tsserver",
+        -- tsserver_path = "tsserver",
+      },
+    })
   end,
 }
